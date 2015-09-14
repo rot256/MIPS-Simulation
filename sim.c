020f946b3ecd56592bb4b4c2116bab114223bc7e
rot256 <rot256@users.noreply.github.com>
2015-09-15 01:20:57 +0200

    Added required instructions for assignment 1
    Added support for the remaining op codes.
    Nothing is tested, assume it is completly broken!

--- sim.c
+++ sim.c
@@ -6,6 +6,7 @@
 #include "mips32.h"
 
 // https://www.cs.umd.edu/class/sum2003/cmsc311/Notes/Mips/format.html
+// http://www.mrc.uidaho.edu/mrc/people/jff/digital/MIPSir.html
 
 // Types
 #define uchar unsigned char
@@ -38,31 +39,31 @@ size_t alloc;
 uchar* mem;
 
 int read_config_stream(FILE* f) {
-	for (int i = 8; i < 16; i++) {
-		if (fscanf(f, "%u", &regs[i]) != 1) {
-			if (errno == 0) return ERROR_INVALID_CONFIG;
-			return ERROR_IO_ERROR;
-		}
+    for (int i = 8; i < 16; i++) {
+	if (fscanf(f, "%u", &regs[i]) != 1) {
+	    if (errno == 0) return ERROR_INVALID_CONFIG;
+	    return ERROR_IO_ERROR;
 	}
-        return 0;
+    }
+    return 0;
 }
 
 void print_status() {
-	printf("Executed %zu instruction(s).\n", instr_cnt);
-	printf("pc = 0x%x\n", IP);
-        printf("at = 0x%x\n", regs[r_at]);
-        printf("v0 = 0x%x\n", regs[r_v0]);
-        printf("v1 = 0x%x\n", regs[r_v1]);
-        for(int i = 0; i < 8; i++) printf("t%d = 0x%x\n", i, regs[i+8]);
-        printf("sp = 0x%x\n", regs[r_sp]);
-        printf("ra = 0x%x\n", regs[r_ra]);
+    printf("Executed %zu instruction(s).\n", instr_cnt);
+    printf("pc = 0x%x\n", IP);
+    printf("at = 0x%x\n", regs[r_at]);
+    printf("v0 = 0x%x\n", regs[r_v0]);
+    printf("v1 = 0x%x\n", regs[r_v1]);
+    for(int i = 0; i < 8; i++) printf("t%d = 0x%x\n", i, regs[i+8]);
+    printf("sp = 0x%x\n", regs[r_sp]);
+    printf("ra = 0x%x\n", regs[r_ra]);
 }
 
 int read_config(const char *path) {
-	FILE* f = fopen(path, "r");
-	if (f == NULL) return ERROR_IO_ERROR;
-	if (read_config_stream(f) != 0) return ERROR_IO_ERROR;
-	return fclose(f);
+    FILE* f = fopen(path, "r");
+    if (f == NULL) return ERROR_IO_ERROR;
+    if (read_config_stream(f) != 0) return ERROR_IO_ERROR;
+    return fclose(f);
 }
 
 int interp_r(uint32_t inst) {
@@ -92,7 +93,7 @@ int interp_r(uint32_t inst) {
             regs[rd] = !(regs[rs] | regs[rt]);
             break;
         case FUNCT_SLT:
-            regs[rd] = regs[rs] < regs[rt];
+            regs[rd] = ((int32_t) regs[rs] < (int32_t) regs[rt]) ? 1 : 0;
             break;
         case FUNCT_SLL:
             regs[rd] = regs[rs] << GET_SHAMT(inst);
@@ -104,8 +105,7 @@ int interp_r(uint32_t inst) {
     return 0;
 }
 
-/*
- *
+/* Tmp comment - remove later
 #define OPCODE_R        (0x00)
 #define OPCODE_J        (0x02)
 #define OPCODE_JAL      (0x03)
@@ -142,17 +142,22 @@ int interp() {
                 if (GET_RS(inst) == GET_RT(inst)) IP += GET_IMM(inst);
                 break;
             case OPCODE_BNE:
+                if (GET_RS(inst) != GET_RT(inst)) IP += GET_IMM(inst);
                 break;
             case OPCODE_ADDIU:
                 regs[GET_RS(inst)] += GET_IMM(inst);
                 break;
             case OPCODE_SLTI:
+                regs[GET_RT(inst)] = ((int32_t) regs[GET_RS(inst)] < (int32_t) GET_IMM(inst)) ? 1 : 0;
                 break;
             case OPCODE_ANDI:
+                regs[GET_RT(inst)] = regs[GET_RS(inst)] & GET_IMM(inst);
                 break;
             case OPCODE_ORI:
+                regs[GET_RT(inst)] = regs[GET_RS(inst)] | GET_IMM(inst);
                 break;
             case OPCODE_LUI:
+                regs[GET_RT(inst)] = GET_IMM(inst) << 16;
                 break;
             case OPCODE_LW:
                 regs[GET_RS(inst)] = GET_BIGWORD(mem, GET_IMM(inst));
@@ -169,58 +174,58 @@ int interp() {
 }
 
 int main(int argc, char* argv[]) {
-	int ret;
+    int ret;
 
-	// Print usage
-	if (argc != 3) {
-		printf("Usage:\n");
-		printf("%s config_file mips_elf_file\n", argv[0]);
-		printf("Error codes:\n");
-		printf("IO ERROR       : %4d\n", ERROR_IO_ERROR);
-		printf("INVALID CONFIG : %4d\n", ERROR_INVALID_CONFIG);
-		exit(0);
-	}
+    // Print usage
+    if (argc != 3) {
+        printf("Usage:\n");
+        printf("%s config_file mips_elf_file\n", argv[0]);
+        printf("Error codes:\n");
+        printf("IO ERROR       : %4d\n", ERROR_IO_ERROR);
+        printf("INVALID CONFIG : %4d\n", ERROR_INVALID_CONFIG);
+        exit(0);
+    }
 
-	// Read config file and print inital status
-	ret = read_config(argv[1]);
-	if (ret != 0) {
-            printf("Failed to load config, with code %d\n", ret);
-            exit(ret);
+    // Read config file and print inital status
+    ret = read_config(argv[1]);
+    if (ret != 0) {
+        printf("Failed to load config, with code %d\n", ret);
+        exit(ret);
+    }
+
+    // Parse elf file (why the F does the elf parser write to stdout?)
+    alloc = KB*64;
+    mem = (uchar*) malloc(alloc);
+    while(1) {
+        ret = elf_dump(argv[2], &IP, mem, alloc);
+        if (ret == ELF_ERROR_OUT_OF_MEM) {
+            alloc *= 2;
+            mem = (uchar*) realloc(mem, alloc);
+            continue;
         }
-
-        // Parse elf file (why the F does the elf parser write to stdout?)
-        alloc = KB*64;
-        mem = (uchar*) malloc(alloc);
-        while(1) {
-            ret = elf_dump(argv[2], &IP, mem, alloc);
-            if (ret == ELF_ERROR_OUT_OF_MEM) {
-                alloc *= 2;
-                mem = (uchar*) realloc(mem, alloc);
-                continue;
-            }
-            if (ret != 0) {
-                printf("Failed to parse elf file!");
-                exit(ERROR_INVALID_ELF);
-            }
-            break;
+        if (ret != 0) {
+            printf("Failed to parse elf file!");
+            exit(ERROR_INVALID_ELF);
         }
+        break;
+    }
 
-        // Set stack pointer
-        regs[29] = alloc + MIPS_RESERVE - sizeof(uint32_t);
-        print_status();
+    // Set stack pointer
+    regs[29] = alloc + MIPS_RESERVE - sizeof(uint32_t);
+    print_status();
 
-        //
-        ret = interp();
-        if (ret == ERROR_UNKNOWN_OPCODE) {
-            printf("Found unknown opcode!\n");
-            exit(ret);
-        } else  if (ret == SIG_HALT_PROGRAM) {
-            printf("Program has stopped (encountered a syscall)\n");
-        }
-        print_status();
+    //
+    ret = interp();
+    if (ret == ERROR_UNKNOWN_OPCODE) {
+        printf("Found unknown opcode!\n");
+        exit(ret);
+    } else  if (ret == SIG_HALT_PROGRAM) {
+        printf("Program has stopped (encountered a syscall)\n");
+    }
+    print_status();
 
-        printf("%s\n", mem);
-        printf("%u\n", IP);
+    printf("%s\n", mem);
+    printf("%u\n", IP);
 
-	return 0;
+    return 0;
 }
