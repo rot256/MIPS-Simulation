c2060fcf9d8d296d1b714909c3ca275b60b270e8
Rot256 <Rot256@users.noreply.github.com>
2015-09-15 13:07:02 +0100

    Slightly less fucked when running branch inst
    Branching might work now

--- sim.c
+++ sim.c
@@ -6,7 +6,7 @@
 #include "mips32.h"
 
 // https://www.cs.umd.edu/class/sum2003/cmsc311/Notes/Mips/format.html
-// http://www.mrc.uidaho.edu/mrc/people/jff/digital/MIPSir.html
+// http://www.mrc.uidaho.edu/mrc/people/jff/digital/MPCSir.html
 
 // Types
 #define uchar unsigned char
@@ -15,10 +15,11 @@
 #define SIG_HALT_PROGRAM 1
 
 // Error codes
-#define ERROR_IO_ERROR -1
+#define ERROR_IO_ERROR       -1
 #define ERROR_INVALID_CONFIG -2
-#define ERROR_INVALID_ELF -3
+#define ERROR_INVALID_ELF    -3
 #define ERROR_UNKNOWN_OPCODE -4
+#define ERROR_UNKNOWN_FUNCT  -5
 
 // Index of special registers
 #define r_at 1
@@ -29,15 +30,37 @@
 
 #define KB 1024
 
+#define INSTRUCTION_SIZE sizeof(uint32_t)
+
 // Registers
 uint32_t regs[32]; // General purpose
-uint32_t IP;       // Instruction pointer - PC fucking sucks
+uint32_t PC;       // Program counter
+uint32_t nPC;      // Next program counter
 size_t instr_cnt;  // Number of executed instructions
 
 // Memory
 size_t alloc;
 uchar* mem;
 
+// Advance the program counter
+static inline void advance_pc(int32_t offset) {
+    PC  =  nPC;
+    nPC  += offset;
+}
+
+// Print simulation status
+void print_status() {
+    printf("Executed %zu instruction(s).\n", instr_cnt);
+    printf("pc = 0x%x\n", PC);
+    printf("at = 0x%x\n", regs[r_at]);
+    printf("v0 = 0x%x\n", regs[r_v0]);
+    printf("v1 = 0x%x\n", regs[r_v1]);
+    for(int i = 0; i < 8; i++) printf("t%d = 0x%x\n", i, regs[i+8]);
+    printf("sp = 0x%x\n", regs[r_sp]);
+    printf("ra = 0x%x\n", regs[r_ra]);
+}
+
+// Read config file stream
 int read_config_stream(FILE* f) {
     for (int i = 8; i < 16; i++) {
 	if (fscanf(f, "%u", &regs[i]) != 1) {
@@ -48,17 +71,7 @@ int read_config_stream(FILE* f) {
     return 0;
 }
 
-void print_status() {
-    printf("Executed %zu instruction(s).\n", instr_cnt);
-    printf("pc = 0x%x\n", IP);
-    printf("at = 0x%x\n", regs[r_at]);
-    printf("v0 = 0x%x\n", regs[r_v0]);
-    printf("v1 = 0x%x\n", regs[r_v1]);
-    for(int i = 0; i < 8; i++) printf("t%d = 0x%x\n", i, regs[i+8]);
-    printf("sp = 0x%x\n", regs[r_sp]);
-    printf("ra = 0x%x\n", regs[r_ra]);
-}
-
+// Read config
 int read_config(const char *path) {
     FILE* f = fopen(path, "r");
     if (f == NULL) return ERROR_IO_ERROR;
@@ -66,6 +79,7 @@ int read_config(const char *path) {
     return fclose(f);
 }
 
+// Interpid R type instruction
 int interp_r(uint32_t inst) {
     uint32_t rs = GET_RS(inst);
     uint32_t rt = GET_RT(inst);
@@ -73,102 +87,162 @@ int interp_r(uint32_t inst) {
     printf("%08x\n", inst);
     printf("Des: %u, Src1: %u Src2: %u\n", rs, rt, rd);
     switch(GET_FUNCT(inst)) {
+        // Jump register
         case FUNCT_JR:
+            PC = nPC;
+            nPC = regs[rs];
             break;
+
+        // Syscall (not implemented)
         case FUNCT_SYSCALL:
             return SIG_HALT_PROGRAM;
+
+        // Add unsigned
         case FUNCT_ADDU:
             regs[rd] = regs[rs] + regs[rt];
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Subtract unsigned
         case FUNCT_SUBU:
             regs[rd] = regs[rs] - regs[rt];
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Bitwise and
         case FUNCT_AND:
             regs[rd] = regs[rs] & regs[rt];
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Bitwise or
         case FUNCT_OR:
             regs[rd] = regs[rs] | regs[rt];
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Bitwise not or
         case FUNCT_NOR:
             regs[rd] = !(regs[rs] | regs[rt]);
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Set on less than
         case FUNCT_SLT:
             regs[rd] = ((int32_t) regs[rs] < (int32_t) regs[rt]) ? 1 : 0;
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Shift left
         case FUNCT_SLL:
             regs[rd] = regs[rs] << GET_SHAMT(inst);
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        // Shift right
         case FUNCT_SRL:
             regs[rd] = regs[rs] >> GET_SHAMT(inst);
+            advance_pc(INSTRUCTION_SIZE);
             break;
+
+        default:
+            return ERROR_UNKNOWN_FUNCT;
     }
     return 0;
 }
 
-/* Tmp comment - remove later
-#define OPCODE_R        (0x00)
-#define OPCODE_J        (0x02)
-#define OPCODE_JAL      (0x03)
-#define OPCODE_BEQ      (0x04)
-#define OPCODE_BNE      (0x05)
-#define OPCODE_ADDIU    (0x09)
-#define OPCODE_SLTI     (0x0A)
-#define OPCODE_ANDI     (0x0C)
-#define OPCODE_ORI      (0x0D)
-#define OPCODE_LUI      (0x0F)
-#define OPCODE_LW       (0x23)
-#define OPCODE_SW       (0x2B)
-*/
-
+// Run simulation
 int interp() {
     int ret;
     uint32_t inst;
     while(1) {
-        inst = GET_BIGWORD(mem, IP);
-        printf("-> %u\n", GET_OPCODE(inst));
+        inst = GET_BIGWORD(mem, PC);
         switch(GET_OPCODE(inst)) {
+            // R type instruction
             case OPCODE_R:
                 ret = interp_r(inst);
                 if (ret != 0) return ret;
                 break;
+
+            // Unconditional jump
             case OPCODE_J:
-                IP = GET_ADDRESS(inst);
+                PC = nPC;
+                nPC = GET_ADDRESS(inst);
                 break;
+
+            // Jump and link
             case OPCODE_JAL:
-                regs[r_ra] = IP + sizeof(uint32_t);
-                IP = GET_ADDRESS(inst);
+                regs[r_ra] = nPC + INSTRUCTION_SIZE;
+                PC = nPC;
+                nPC = GET_ADDRESS(inst);
                 break;
+
+            // Branch on equal
             case OPCODE_BEQ:
-                if (GET_RS(inst) == GET_RT(inst)) IP += GET_IMM(inst);
+                if (GET_RS(inst) == GET_RT(inst)) {
+                    advance_pc(GET_IMM(inst) << 2);
+                } else {
+                    advance_pc(INSTRUCTION_SIZE);
+                }
                 break;
+
+            // Branch on not equal
             case OPCODE_BNE:
-                if (GET_RS(inst) != GET_RT(inst)) IP += GET_IMM(inst);
+                if (GET_RS(inst) != GET_RT(inst)) {
+                    advance_pc(GET_IMM(inst) << 2);
+                } else {
+                    advance_pc(INSTRUCTION_SIZE);
+                }
                 break;
+
+            // Add immediate unsigned (no overflow)
             case OPCODE_ADDIU:
                 regs[GET_RS(inst)] += GET_IMM(inst);
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Set on less than (signed)
             case OPCODE_SLTI:
                 regs[GET_RT(inst)] = ((int32_t) regs[GET_RS(inst)] < (int32_t) GET_IMM(inst)) ? 1 : 0;
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Bitwise and on immediate
             case OPCODE_ANDI:
                 regs[GET_RT(inst)] = regs[GET_RS(inst)] & GET_IMM(inst);
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Bitwise or on immediate
             case OPCODE_ORI:
                 regs[GET_RT(inst)] = regs[GET_RS(inst)] | GET_IMM(inst);
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Load upper immediate
             case OPCODE_LUI:
                 regs[GET_RT(inst)] = GET_IMM(inst) << 16;
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Load word
             case OPCODE_LW:
                 regs[GET_RS(inst)] = GET_BIGWORD(mem, GET_IMM(inst));
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
+            // Save word
             case OPCODE_SW:
                 SET_BIGWORD(mem, GET_RS(inst), regs[GET_RT(inst)]);
+                advance_pc(INSTRUCTION_SIZE);
                 break;
+
             default:
                 return ERROR_UNKNOWN_OPCODE;
         }
-        IP+=sizeof(uint32_t);
+
+        // Ensure that zero = 0
+        regs[0] = 0;
         instr_cnt++;
     }
 }
@@ -197,7 +271,7 @@ int main(int argc, char* argv[]) {
     alloc = KB*64;
     mem = (uchar*) malloc(alloc);
     while(1) {
-        ret = elf_dump(argv[2], &IP, mem, alloc);
+        ret = elf_dump(argv[2], &PC, mem, alloc);
         if (ret == ELF_ERROR_OUT_OF_MEM) {
             alloc *= 2;
             mem = (uchar*) realloc(mem, alloc);
@@ -214,7 +288,10 @@ int main(int argc, char* argv[]) {
     regs[29] = alloc + MIPS_RESERVE - sizeof(uint32_t);
     print_status();
 
-    //
+    // Set next program counter
+    nPC = PC + INSTRUCTION_SIZE;
+
+    // Run
     ret = interp();
     if (ret == ERROR_UNKNOWN_OPCODE) {
         printf("Found unknown opcode!\n");
@@ -225,7 +302,7 @@ int main(int argc, char* argv[]) {
     print_status();
 
     printf("%s\n", mem);
-    printf("%u\n", IP);
+    printf("%u\n", PC);
 
     return 0;
 }
